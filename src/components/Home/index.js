e2f75779790d1a131e0f3643fac121237ba3b481
Marvin Merida <marvinmerida@Marvins-MBP.fios-router.home>
2022-07-29 11:46:39 -0400

    Styling added to the text on my home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,19 @@
 import LogoTitle from '../../assets/images/M_logo.png'
+import { Link } from 'react-router-dom';
+import './index.scss';
 
 const Home = () => {
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br /> I'm </h1>
-                <img scr={LogoTitle} alt="developer"/>
+                <h1>Hi, <br /> I'm
+                    <img scr={LogoTitle} alt="developer" />
+                    arvin
+                    <br />
+                    web developer
+                </h1>
+                <h2>Software Developer / JavaScript Expert</h2>
+                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
         </div>
     );
